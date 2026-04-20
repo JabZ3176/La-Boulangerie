030aac6c761302f21ff88d3fc6e49b245f4284ed
JabZ3176 <jordan.delport24@gmail.com>
2026-04-20 16:42:31 +0200

    UI Commit

--- La Boulangerie/Assets/Scripts/Player.cs
+++ La Boulangerie/Assets/Scripts/Player.cs
@@ -1,21 +1,28 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public int health = 3;
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
     public float jumpCutMultiplier = 0.5f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
+    public Image healthImage;
 
 
     private Rigidbody2D rb;
     private bool isJumping;
-    private bool isGrounded; 
+    private bool isGrounded;
+    
+    private SpriteRenderer spriteRenderer;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -36,10 +43,38 @@ public class Player : MonoBehaviour
             }
             isJumping = false;
         }
+
+        healthImage.fillAmount = health / 3f;
     }
 
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Damage") 
+        {
+            health -= 1;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            StartCoroutine(BlinkRed());
+
+            if(health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    private IEnumerator BlinkRed()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = Color.white;
+    }
+    private void Die()
+    {
+       UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
     }
 }
